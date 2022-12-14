65033e8633db0169b08639f751d1cd62913203bd
Igor Shmidt <schmidt.igor2012@yandex.ru>
2022-12-14 12:54:01 +0300

    feat: реализовать валидатор для User

--- filmorate/src/main/java/controller/Validators/UserRequestValidator.java
+++ filmorate/src/main/java/controller/Validators/UserRequestValidator.java
@@ -1,4 +1,25 @@
 package controller.Validators;
 
-public class UserRequestValidator {
+import module.Exceptions.Invalid.UserInvalidException;
+import module.User;
+
+import java.time.Instant;
+import java.time.LocalDate;
+
+public class UserRequestValidator implements Validator<User>{
+
+    @Override
+    public void valid(User user) throws UserInvalidException {
+        StringBuilder error = new StringBuilder();
+        if (user.getEmail().isBlank() || !user.getEmail().contains("@"))
+            error.append("Электронная почта не может быть пустой и должна содержать символ @.\n");
+        if (user.getLogin().isBlank() || user.getLogin().contains(" "))
+            error.append("Логин не может быть пустым и содержать пробелы\n");
+        if (user.getName().isBlank())
+            user.setName(user.getLogin());
+        if (user.getBirthday().isAfter(LocalDate.from(Instant.now())))
+            error.append("Дата рождения не может быть в будущем.\n");
+        if (error.toString().isEmpty())
+            throw new UserInvalidException(error.toString());
+    }
 }
